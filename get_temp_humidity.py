7cdbf64664849f7fc8ad81ae349bae419979d263
Pooya Eslami <pooyae@gmail.com>
2017-06-28 15:54:32 -0400

    added pyfiglet

--- get_temp_humidity.py
+++ get_temp_humidity.py
@@ -2,9 +2,18 @@ from PIGPIO import pigpio
 pi = pigpio.pi()
 import DHT22
 import time
+from pyfiglet import Figlet
+f=Figlet(font='banner3')
 s=DHT22.sensor(pi,2)
 while True:
     s.trigger()
     time.sleep(10)
-    now = time.strftime("%c")
-    print('Temperature: {:3.2f} F'.format(s.temperature()*1.8+32)+', Humidity: {:3.2f}%'.format(s.humidity()/1.)+' on '+now)
+    nowT = time.strftime("%H:%M:%S")
+    nowD = time.strftime("%m-%d-%y")
+    print(chr(27)+"[2J")
+    print(f.renderText('  '+nowD))
+    print(f.renderText('  '+nowT))
+    print("\n"*10)
+    print(f.renderText('  T: {:3.0f} F'.format(s.temperature()*1.8+32)))
+    print(f.renderText('  H: {:3.0f}%'.format(s.humidity()/1.)))
+    print("\n"*10)
